e66014f371ce4c9480f0aec79b0ac01c5c8a2e36
Bryan P. Galligan, SJ <bgalligan@jesuits.org>
2024-05-14 11:08:44 -0400

    Created script for variable selection

---
+++ 04_select-variables.R
@@ -0,0 +1,18 @@
+## This script uses the RFM algorithm to select
+## predictor variables
+
+##### Fyke99 - Water Temp * Bin #####
+
+##### Fyke 99 - Water Temp * Freq #####
+
+##### Fyke 99 - Air Temp * Bin #####
+
+##### Fyke 99 - Air Temp * Freq #####
+
+##### Fyke 19 - Water Temp * Bin #####
+
+##### Fyke 19 - Water Temp * Freq #####
+
+##### Fyke 19 - Air Temp * Bin #####
+
+##### Fyke 19 - Air Temp * Freq #####
